ea319735085d89e8f22d09b1d76d6ef11bf8c103
jaketrent <trent.jake@gmail.com>
2018-01-01 08:37:20 -0700

    add static 200 and / to serve /index.html

--- static/routes.go
+++ static/routes.go
@@ -2,10 +2,12 @@ package static
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func mountFile(router *gin.Engine, fileName string) {
 	router.GET(fileName, func(c *gin.Context) {
+		c.Status(http.StatusOK)
 		c.File("./client/build" + fileName)
 	})
 }
@@ -14,6 +16,10 @@ func Mount(router *gin.Engine) {
 	mountFile(router, "/asset-manifest.json")
 	mountFile(router, "/favicon.ico")
 	mountFile(router, "/index.html")
+	router.GET("/", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+		c.File("./client/build/index.html")
+	})
 	mountFile(router, "/manifest.json")
 	mountFile(router, "/service-worker.js")
 	router.Static("/static", "./client/build/static")
