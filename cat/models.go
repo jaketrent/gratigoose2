d59de3fc7926162f526cc29dc7e8621cc97c9918
jaketrent <trent.jake@gmail.com>
2018-10-31 07:19:47 -0600

    allow null in cat description string from db
    To avoid the error:
    cat list error sql: Scan error on column index 3, name "description": unsupported Scan, storing driver.Value type <nil> into type *string
    
    https://medium.com/aubergine-solutions/how-i-handled-null-possible-values-from-database-rows-in-golang-521fb0ee267

--- cat/models.go
+++ cat/models.go
@@ -1,6 +1,8 @@
 package cat
 
 import (
+	"database/sql"
+	"encoding/json"
 	"time"
 )
 
@@ -8,11 +10,22 @@ import (
 // https://stackoverflow.com/questions/23917916/printable-enums-in-go
 
 type Cat struct {
-	Id          int       `json:"id"`
-	Name        string    `json:"name"`
-	Abbrev      string    `json:"abbrev"`
-	Description string    `json:"description"`
-	Type        string    `json:"type"`
-	Created     time.Time `json:"created"`
-	Updated     time.Time `json:"updated"`
+	Id          int        `json:"id"`
+	Name        string     `json:"name"`
+	Abbrev      string     `json:"abbrev"`
+	Description NullString `json:"description"`
+	Type        string     `json:"type"`
+	Created     time.Time  `json:"created"`
+	Updated     time.Time  `json:"updated"`
+}
+
+type NullString struct {
+	sql.NullString
+}
+
+func (ns *NullString) MarshalJSON() ([]byte, error) {
+	if !ns.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ns.String)
 }
