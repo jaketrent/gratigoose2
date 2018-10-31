762c04b917bcd79c32c80555699e2f122d60b5f2
jaketrent <trent.jake@gmail.com>
2018-10-31 07:22:46 -0600

    test: add seed data for db

---
+++ db/insert_test_data.sql
@@ -0,0 +1,11 @@
+-- acct
+insert into acct
+(name, abbrev)
+values ('Checking', 'mach')
+, ('Savings', 'mas');
+
+-- cat
+insert into cat
+(name, abbrev, type)
+values ('Work', 'wk', 'credit')
+, ('Groceries', 'gro', 'debit');
